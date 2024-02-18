b951112bde9cd190704703ba4d285c1b5f231d30
nadazkai21 <nadazaki019@gmail.com>
2024-02-18 21:45:01 +0200

    radix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -78,11 +78,8 @@ void radix_sort(int *array, size_t size)
 	size_t i, no_of_digits = 0, all_same_number = 1, j;
 	int *array_copy, *remainder_array, power = 1, max;
 
-	if (size == 1)
-	{
-		print_array(array, size);
+	if (size == 1 || array == NULL)
 		return;
-	}
 	for (i = 0; i < size - 1; i++)
 	{
 		if (array[i] != array[i + 1])
