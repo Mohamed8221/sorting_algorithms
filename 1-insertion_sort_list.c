fcb0a2da625b06a01d31ae572befeeec1f14925b
nadazkai21 <nadazaki019@gmail.com>
2024-02-19 11:27:21 +0200

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,24 @@
 #include "sort.h"
-/*
-* insertion_sort_list - function that does the insertion sort method
-* @list: the list to be sorted
+/**
+ * swap_nodes - function that swaps 2 nodes
+ * @node1: node 1
+ * @node2: node 2
 */
-
-
+void swap_nodes(listint_t **node1, listint_t **node2)
+{
+	(*node1)->next = (*node2)->next;
+	(*node2)->next = *node1;
+	if ((*node1)->next != NULL)
+		(*node1)->next->prev = *node1;
+	(*node2)->prev = (*node1)->prev;
+	(*node1)->prev = *node2;
+	if ((*node2)->prev != NULL)
+		(*node2)->prev->next = *node2;
+}
+/**
+ * insertion_sort_list - function that sorts usuing the insertion sort method
+ * @list: the doubly linked list to be sorted
+*/
 void insertion_sort_list(listint_t **list)
 {
 	int sorted_flag = 0, swaped;
@@ -12,13 +26,8 @@ void insertion_sort_list(listint_t **list)
 
 	if (*list == NULL || list == NULL)
 	return;
-
 	if ((*list) != NULL && (*list)->next == NULL)
-	{
-		/*print_list(*list);*/
 		return;
-	}
-		
 	while (sorted_flag != 1)
 	{
 		swaped = 0;
@@ -31,27 +40,13 @@ void insertion_sort_list(listint_t **list)
 			{
 				node1 = copy;
 				node2 = copy->next;
-				node1->next = node2->next;
-				node2->next = node1;
-				if (node1->next != NULL)
-					node1->next->prev = node1;
-				node2->prev = node1->prev;
-				node1->prev = node2;
-				if (node2->prev != NULL)
-					node2->prev->next = node2;
+				swap_nodes(&node1, &node2);
 			}
 			else
 			{
 				node1 = copy;
 				node2 = copy->next;
-				node1->next = node2->next;
-				node2->next = node1;
-				if (node1->next != NULL)
-					node1->next->prev = node1;
-				node2->prev = node1->prev;
-				node1->prev = node2;
-				if (node2->prev != NULL)
-					node2->prev->next = node2;
+				swap_nodes(&node1, &node2);
 				*list = node2;
 				copy = node2;
 			}
