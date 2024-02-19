e10c6e0c02eb4fb458a2840c58998ec5614b54a6
nadazkai21 <nadazaki019@gmail.com>
2024-02-19 11:01:50 +0200

    modifying selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,32 +8,33 @@
 
 void selection_sort(int *array, size_t size)
 {
-int smallest, tmp, swaped;
-size_t i, j, index_of_smallest;
+	int smallest, tmp, swaped;
+	size_t i, j, index_of_smallest;
 
-for (i = 0; i < size; i++)
-{
-smallest = array[i];
-swaped = 0;
-for (j = i + 1 ; j < size; j++)
-{
-if (smallest > array[j])
-{
-smallest = array[j];
-index_of_smallest = j;
-swaped = 1;
-}
+	if (size < 2 || array == NULL)
+		return;
 
-}
-
-if (swaped == 1)
-{
-tmp = array[i];
-array[i] = array[index_of_smallest];
-array[index_of_smallest] = tmp;
-print_array(array, size);
-}
-if (swaped == 0 && i == size - 2)
-break;
-}
+	for (i = 0; i < size; i++)
+	{
+		smallest = array[i];
+		swaped = 0;
+		for (j = i + 1 ; j < size; j++)
+		{
+			if (smallest > array[j])
+			{
+			smallest = array[j];
+			index_of_smallest = j;
+			swaped = 1;
+			}
+		}
+		if (swaped == 1)
+		{
+			tmp = array[i];
+			array[i] = array[index_of_smallest];
+			array[index_of_smallest] = tmp;
+			print_array(array, size);
+		}
+		if (swaped == 0 && i == size - 2)
+		break;
+	}
 }
